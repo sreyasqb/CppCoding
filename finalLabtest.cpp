527c7e2875a3e09083fcfb1bd82129b634970f3f
sreyasqb <20pt33@psgtech.ac.in>
2021-11-20 15:42:42 +0530

    final test done

--- finalLabtest.cpp
+++ finalLabtest.cpp
@@ -2,12 +2,13 @@
 using namespace std;
 class Node{
     public:
-        int data,npl;
+        int npl;
+        double data;
         string name;
         Node *left,*right;
-        Node(int d,string n,Node *l=NULL,Node *r=NULL,int np=0) :data(d),name(n),left(l),right(r),npl(np){};
+        Node(double d,string n,Node *l=NULL,Node *r=NULL,int np=0) :data(d),name(n),left(l),right(r),npl(np){};
         void printNode(){
-
+            cout<<name<< " "<<data<<endl;
         }
 
 };
@@ -17,6 +18,9 @@ class LeftistHeap{
         Node *root;
         LeftistHeap():root(NULL){};
         //to swap children if npl is not satisfied
+        bool isEmpty(){
+            return root==NULL;
+        }
         void swapChildren(Node *t){
             Node *temp=t->left;
             t->left=t->right;
@@ -46,12 +50,20 @@ class LeftistHeap{
             return h1;
         }
         //insert function
-        void insert(int data,string name){
+        void insert(double data,string name){
             root=merge(root,new Node(data,name));
         }
+        //to get the root node which is max
         void printMax(){
+            if (isEmpty()){
+                cout<<"Heap is empty!!!"<<endl;
+                return ;
+            }
             Node *oldroot=root;
             root=merge(root->left,root->right);
+            oldroot->printNode();
+            
+            delete oldroot;
             
         }
 
@@ -59,19 +71,65 @@ class LeftistHeap{
 };
 
 int main(){
-    LeftistHeap h;
-    // h.insert(5);
-    // h.insert(1);
-    // h.insert(9);
-    // h.insert(2);
-    // h.insert(7);
-    // LeftistHeap h1;
-    // h1.insert(6);
-    // h1.insert(1);
-    // h1.insert(9);
-    // cout<<h1.root->data;
-    // h.merge(h1);
-    // h.insert(0);
-     
-    cout<<h.root->left->data<<endl;
+    
+    
+    LeftistHeap CS,IT,Both,IT2;
+    int n;
+
+    cout<<endl<<"Enter the number of students in CS branch : ";
+    cin>>n;
+    
+    cout<<endl<<"For CS branch : "<< endl;
+    for (int i=0; i<n; i++){
+        string name;
+        double cgpa;
+        cout<<endl<<"Enter name and cgpa seperated by spaces for student "<<i+1<<endl;
+        cin>>name>>cgpa; //taking input 
+        CS.insert(cgpa,name); //inserting into CS heap
+        Both.insert(cgpa,name);
+    }
+    cout<<endl<<"Enter the number of students in IT branch : ";
+    cin>>n;
+    
+    cout<<endl<<"For IT branch : "<< endl;
+    for (int i=0; i<n; i++){
+        string name;
+        double cgpa;
+        cout<<endl<<"Enter name and cgpa seperated by spaces for student "<<i+1<<endl;
+        cin>>name>>cgpa; //taking input 
+        
+        IT.insert(cgpa,name); //inserting into CS heap
+        IT2.insert(cgpa,name);
+    }
+    cout<<endl<<endl;
+
+    Both.merge(IT2);
+
+    
+    int n1;
+    string choice;
+    cout<<"Enter number of students to find best cgpa : ";
+    cin>>n1;
+    cout<<"Enter CS to find top " <<n1<< " students of CS"<<endl;
+    cout<<"Enter IT to find top " <<n1<< " students of IT"<<endl;
+    cout<<"Enter Both to find top " <<n1<< " students of Both"<<endl;
+    cout<<"Enter : ";
+    cin>>choice;
+    cout<<endl<<"The top "<<n1<< " Students of " << choice<< " Are : "<<endl<<endl;
+    if (choice=="CS")
+        for (int i=0; i<n1; i++)
+            CS.printMax();
+        
+    else if (choice=="IT")
+        for (int i=0;i<n1;i++)
+            IT.printMax();
+        
+
+    else if (choice=="Both")
+        for (int i=0;i<n1;i++)
+            Both.printMax();
+
+    cout<<endl;
+    
+
 }
