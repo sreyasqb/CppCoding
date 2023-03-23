82ad71a3b60de61ad938be54044e45286674ee16
Sreyas S <76519139+sreyasqb@users.noreply.github.com>
2023-03-23 15:53:17 +0530

    Update mandlebrot.py

--- computer_graphics/submissions/mandlebrot.py
+++ computer_graphics/submissions/mandlebrot.py
@@ -1,29 +1,27 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def mandelbrot(c, z):
-   iterations = 500
-   count = 0
-   for a in range(iterations):
+def calc(c, z):
+   j=0
+   for i in range(500):
       z = z**2 + c
-      count += 1        
-      if(abs(z) > 4):
-         break
-   return count
+      j += 1        
+      if(abs(z) > 4):break
+   return j
 
-def mandelbrot_set(x, y):
+def mandelbrot(x, y):
    m = np.zeros((len(x), len(y)))
    for i in range(len(x)):
       for j in range(len(y)):
          c = complex(x[i], y[j])
          z = complex(0, 0)
-         count = mandelbrot(c, z)
+         count = calc(c, z)
          m[i, j] = count    
    return m
 
 x = np.linspace(-2, 1, 1000)
 y = np.linspace(-1, 1,1000)
-m = mandelbrot_set(x, y)
-plt.imshow(m.T, cmap = "magma")
+m = mandelbrot(x, y)
+plt.imshow(m.T, cmap = "RdGy")
 plt.axis("off")
 plt.show()
